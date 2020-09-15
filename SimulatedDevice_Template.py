b6b961523b3e4502ebc17670b4202c93187fc541
XploreTeam <65662725+HackathonXplore15Sep@users.noreply.github.com>
2020-09-15 12:32:03 +0530

    Add files via upload

---
+++ SimulatedDevice_Template.py
@@ -0,0 +1,53 @@
+# program sends FieldId, Date, GatewayId, DeviceId to IoT Hub
+#
+
+
+import random
+import time
+
+from azure.iot.device import IoTHubDeviceClient, Message
+
+CONNECTION_STRING ="HostName=demotesting.azure-devices.net;DeviceId=demodevice;SharedAccessKey=qDs1eneb9rzmCBD/6e52/2yZ/ODE8PJXPgTzT7f2qa4="
+MSG_TXT='{{"FieldId":"{FieldId}","Date":{Date},"GatewayId":"{GatewayId}","DeviceId":"{DeviceId}"}}'
+ 
+def iothub_client_init():
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
+
+def iothub_client_telemetry_sample_run():
+
+    try:
+        client = iothub_client_init()
+        input1 = open('input.csv', 'r')
+        
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+        messageId=0
+
+        Lines = input1.readlines()
+        for line in Lines:
+            print(line)
+            x = line.split(',')
+                 
+            FieldId=str(x[0])
+            Date=x[1]
+            GatewayId = str(x[2])
+            DeviceId=str(x[3])
+
+
+            msg_txt_formatted = MSG_TXT.format(FieldId=FieldId,Date=Date,
+                                GatewayId=GatewayId,DeviceId=DeviceId)
+            message = Message(msg_txt_formatted)
+          
+            print( "Sending message: {}".format(message) )
+            client.send_message(message)
+            print(message)
+            print ( "Message successfully sent" )
+            time.sleep(3)
+
+    except OSError as e:
+            print('Error: ' + str(e))
+            
+if __name__ == '__main__':
+    print ( "IoT Hub Simulated device Template" )
+    print ( "Press Ctrl-C to exit" )
+    iothub_client_telemetry_sample_run()
